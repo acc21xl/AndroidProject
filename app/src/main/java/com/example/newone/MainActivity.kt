5873ab8d250860b7caad4609114a5bc8556c88e7
acc21xl <1140422204@qq.com>
2023-08-04 17:03:57 +0800

    some updates

--- app/src/main/java/com/example/newone/MainActivity.kt
+++ app/src/main/java/com/example/newone/MainActivity.kt
@@ -15,7 +15,7 @@ import com.example.newone.ui.theme.NewOneTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+        setContent { //aa
             NewOneTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
