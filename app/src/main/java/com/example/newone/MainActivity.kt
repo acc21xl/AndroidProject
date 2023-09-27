80a558b73c2137b340c16e7bb42f5bcea4b84cbd
acc21xl <1140422204@qq.com>
2023-09-27 22:56:22 +0100

    some updatess

--- app/src/main/java/com/example/newone/MainActivity.kt
+++ app/src/main/java/com/example/newone/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.newone
 
+import android.graphics.Paint.Align
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,37 +14,70 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.newone.ui.theme.NewOneTheme
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.foundation.Image
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { //aa
+        setContent { //aabb
             NewOneTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    GreetingText(message = "Happy Birthday Sam!", from = "From Emma")
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GreetingText(message: String, from : String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.padding(8.dp),   //间距为8
+        verticalArrangement = Arrangement.Center //used to let the comment in the center
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
 }
 
-@Preview(showBackground = true)
+
 @Composable
-fun GreetingPreview() {
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
+    val image = painterResource(R.drawable.androidparty)
+    androidx.compose.foundation.Image(painter = image, contentDescription = null)
+}
+
+
+@Preview(showBackground = false, showSystemUi = true)
+@Composable
+fun BirthdayCardPreview() {
     NewOneTheme {
-        Greeting("Android")
+//        Greeting("James")
+        GreetingImage(message = "Happy Birthday Bur9er!",from = "From Xin")
     }
 }
